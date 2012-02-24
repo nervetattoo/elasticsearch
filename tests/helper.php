24c721186a2691da13efcf84511ecbfd5f4a8f77
Raymond Julin <raymond.julin@gmail.com>
2012-02-23 23:26:41 -0300

    Finally the license is included and named. Fixes #9

--- tests/helper.php
+++ tests/helper.php
@@ -1,4 +1,12 @@
-<?php
+<?php // vim:set ts=4 sw=4 et:
+/**
+ * This file is part of the ElasticSearch PHP client
+ *
+ * (c) Raymond Julin <raymond.julin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 $path = split("/",pathinfo(__FILE__, PATHINFO_DIRNAME));
 array_pop($path);
