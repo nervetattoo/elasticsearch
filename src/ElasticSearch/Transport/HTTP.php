7736dabb10977b3a831b82e1ed4ac6e37d397dda
Björn Bösel <bjoernboesel@gmail.com>
2013-08-21 12:24:23 +0200

    Stupid syntax error

--- src/ElasticSearch/Transport/HTTP.php
+++ src/ElasticSearch/Transport/HTTP.php
@@ -131,7 +131,7 @@ class HTTP extends Base {
      */
     public function delete($id=false, array $options = array()) {
         if ($id)
-            return $this->call($this->buildUrl(array($this->type, $id), $options), "DELETE") );
+            return $this->call($this->buildUrl(array($this->type, $id), $options), "DELETE");
         else
             return $this->request(false, "DELETE");
     }
